43d22fe2b2699ebbe7a398ecf84d3f61f3993fad
Renato Rodrigues <99916443+NatoRodrigues@users.noreply.github.com>
2022-11-30 22:57:37 -0300

    projeto finalizado

---
+++ RD-Home-Page/js/script.js
@@ -0,0 +1,10 @@
+
+
+window.scroll(0, 100);
+
+function abreEmail() {
+    window.location.href('mailto:rrsilva.comerciolocacoes@hotmail.com');
+}
+function abreZap(){
+    window.open("https://api.whatsapp.com/send?phone=5581983472924","width=200,height=100");
+}
